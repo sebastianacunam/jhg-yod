8e68c44755700131a76aa0b998031f0e8f40fb5a
sebastianacunam <61118493+sebastianacunam@users.noreply.github.com>
2024-01-11 20:11:07 -0300

    form register almost done

--- frontend/src/components/CommonComponents/FormRegister/FormRegister.jsx
+++ frontend/src/components/CommonComponents/FormRegister/FormRegister.jsx
@@ -1,84 +1,97 @@
-import { useState, React} from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router'
-import swal from 'sweetalert'
-import validateEmail from '../../../middleware/validateEmail'
-import validatePassword from '../../../middleware/validatePassword'
-import { registerUser } from '../../../redux/actions/actionUser'
-import '../../../assets/scss/layout/_formRegister.scss'
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
+import swal from 'sweetalert';
+import { registerUser } from '../../../redux/actions/actionUser';
+import validateRegister from '../../../middleware/validateRegister';
+import { Link } from 'react-router-dom'
+import '../../../assets/scss/layout/_formRegister.scss';
 
 export default function FromRegister() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const [errors, setErrors] = useState({});
+  const [estado, setEstado] = useState({
+    email: '',
+    name: '',
+    password1: '',
+  });
 
-    const [estado, setEstado] = useState({
-        email: "",
-        name: "",
-        password1: "",
-    })
-    const [state, setEstate] = useState(false)
+  const handleSubmitR = (e) => {
+    e.preventDefault();
 
-    const [errors, setErrors] = useState([])
-
-    const handleSubmitR = (e) => {
-        e.preventDefault()
-    
-        if (
-          !estado.email ||
-          !estado.name ||
-          !estado.password1
-        )
-          setErrors([0, "Hay campos vacíos"])
-        else if (estado?.name.length < 3 || estado?.name.length > 40)
-          setErrors([1, "El nombre de usuario debe tener entre 3 y 10 caracteres"])
-        else if (validateEmail(estado?.email)) setErrors([2, "E-mail inválido"])
-        else if (validatePassword(estado?.password1))
-          estado?.password1.length < 8
-            ? setErrors([3, "Tu password debe tener al menos 8 caracteres"])
-            : setErrors([3, "Password inválido"])
-        else {
-          setErrors([])
-          dispatch(registerUser(estado))
-          swal("", "Registrado con éxito, confirma tu email!", "success");
-          navigate("/")
-        }
+    if (Object.values(errors).some((error) => error !== '')) {
+      alert('Faltan campos por completar.');
+    } else {
+      swal('', 'Registrado con éxito, confirma tu email', 'success');
+      dispatch(registerUser(estado));
+      setEstado({
+        email: '',
+        name: '',
+        password1: '',
+      });
+      navigate('/')
     }
+  };
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        setEstado({
-            ...estado,
-            [e.target.name]: e.target.value,
-        })
+  const handleChange = (e) => {
+    e.preventDefault();
 
-        setErrors({
-          ...errors,
-          name: '',
-          email: '',
-          password1: ''
-      })
-        console.log(e.target.value)
+    setEstado({
+      ...estado,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (Object.values(estado).some((value) => value !== '')) {
+      setErrors(validateRegister(estado));
     }
+  }, [estado]);
 
 
-    return (
-        <div className='bg-form-register'>
-            <section className='container-form-register'>
-              <div className='info-register'>
-                <div className='info-container'>
-                  <h4>Registrate</h4>
-                  <p>Registrate con tu correo y contraseña</p>
-                </div>
-                <form className='form-register' onSubmit={handleSubmitR}>
-                    <input className='input-register' type="text" name='name' onChange={handleChange} value={estado?.name} id="name" placeholder='Nombre'/>
-                    <input className='input-register' type="text" name="email" onChange={handleChange} value={estado?.email} id="email" placeholder='Email'/>
-                    <input className='input-register' type="text" name="password1" onChange={handleChange} value={estado?.password1} id="password1" placeholder='Password'/>
-                    <button className='btn-register'>registrarse</button>
-                </form>
-              
+  return (
+    <div className="bg-form-register">
+      <section className="container-form-register">
+        <div className="info-register">
+          <div className="info-container">
+            <h4>Registrate</h4>
+            <p>Registrate con tu correo y contraseña</p>
+          </div>
+          <form className="form-register" onSubmit={handleSubmitR}>
+            <div className='input-group'>
+              <input className="input-register" required type="text" id='name' name="name" onChange={handleChange} value={estado.name}/>
+              <label className='label-register' htmlFor="name">Nombre</label>
+              {errors.name && <p className='register-form-error'>{errors.name}</p>}
+            </div>
+            
+            <div className='input-group'>
+              <input className="input-register" required type="text" id='email' name="email" onChange={handleChange} value={estado.email}/>
+              <label className='label-register' htmlFor="name">Email</label>
+              {errors.email && <p className='register-form-error'>{errors.email}</p>}
+            </div>
+
+            <div className='input-group'>
+              <input className="input-register" required type="text" id='password1' name="password1" onChange={handleChange} value={estado.password1}/>
+              <label className='label-register' htmlFor="name">Password</label>
+              {errors.password1 && <p className='register-form-error'>{errors.password1}</p>}
+            </div>
+
+            <button className="btn-register">Registrarse</button>
+            <div className='box-login-register'>
+              <div>
+              <p className='info-login-register'>¿ya tienes una cuenta?&nbsp;&nbsp;</p> 
               </div>
-            </section>
+              <div>
+                <Link to='/login'>
+                  <p className='info-login-register2'>Logueate</p> 
+                </Link>  
+              </div>
+            </div>
+          </form>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
