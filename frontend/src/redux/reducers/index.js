10676a20c9749f2b5a452377d1e4b99007cb2aa9
enzosamojedny <enzosamojedny@gmail.com>
2024-02-07 16:40:20 -0300

    feat: POST mentoria y GET mentoria funcionales

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,27 +1,30 @@
-import { 
+import {
   //usuarios
-  // GOOGLE_LOGIN, 
-  LOGIN_USER, 
-  LOGOUT_USER, 
-  AUTH_USER, 
-  RESET_ERROR_LOGUIN_USER, 
-  VALIDATE_USER, 
-  SEND_EMAIL_TO_RESET_PASSWORD, 
-  RESET_PASSWORD, 
-  RESET_ERROR, 
-  IS_ADMIN, 
-  UPDATE_NOMBRE, 
-  BORRAR_USUARIO, 
+  // GOOGLE_LOGIN,
+  LOGIN_USER,
+  LOGOUT_USER,
+  AUTH_USER,
+  RESET_ERROR_LOGUIN_USER,
+  VALIDATE_USER,
+  SEND_EMAIL_TO_RESET_PASSWORD,
+  RESET_PASSWORD,
+  RESET_ERROR,
+  IS_ADMIN,
+  UPDATE_NOMBRE,
+  BORRAR_USUARIO,
   ACTUAL,
   //cursos
-  GET_CURSOS
-} from '../utils/constants'
+  GET_CURSOS,
+  //mentorias
+  GET_MENTORIAS,
+  POST_MENTORIAS,
+} from "../utils/constants";
 
 const initialState = {
   usuario: [],
   email: [],
   loginUser: false,
-  usuarioActual: [],   
+  usuarioActual: [],
   allUsuarios: [],
   confirmacion: {},
   invalidToken: true,
@@ -29,12 +32,12 @@ const initialState = {
   updateNombre: [],
   borraUsuario: [],
   cursos: [],
-  allCursos: []
-}
+  allCursos: [],
+  allMentorias: [],
+};
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
-
     //---------------------USER----------------------------------------
     // case GOOGLE_LOGIN:
     //   console.log(action.payload)
@@ -49,7 +52,7 @@ function rootReducer(state = initialState, action) {
         usuario: !action.payload.error ? action.payload : null,
         email: action.payload.error ? action.payload.error : null,
         loginUser: action.payload._id && true,
-      }
+      };
 
     case LOGOUT_USER:
       return {
@@ -61,285 +64,88 @@ function rootReducer(state = initialState, action) {
         email: [],
         invalidToken: true,
         loginUser: false,
-      }
+      };
 
     case AUTH_USER:
       return {
         ...state,
         usuario: action.payload,
-      }
+      };
 
     case RESET_ERROR_LOGUIN_USER:
       return {
         ...state,
         email: action.payload,
-      }
+      };
 
     case VALIDATE_USER:
       return {
         ...state,
         confirmacion: action.payload,
-      }
+      };
 
     case SEND_EMAIL_TO_RESET_PASSWORD:
       return {
         ...state,
         email: action.payload,
-      }
+      };
 
     case RESET_PASSWORD:
       return {
         ...state,
         email: action.payload,
-      }
+      };
 
     case RESET_ERROR:
       return {
         ...state,
         email: [],
-      }
+      };
 
     case IS_ADMIN:
       return {
         ...state,
         isAdmin: action.payload,
-      }
+      };
     case UPDATE_NOMBRE:
       return {
         ...state,
-        updateNombre: action.payload
-      }
+        updateNombre: action.payload,
+      };
     case BORRAR_USUARIO:
       return {
         ...state,
-        borraUsuario: action.payload
-      }
+        borraUsuario: action.payload,
+      };
     case ACTUAL:
       return {
         ...state,
         usuarioActual: action.payload,
-      }
+      };
     //---------------------CURSOS----------------------------------------
     case GET_CURSOS:
       return {
         ...state,
         cursos: action.payload,
         allCursos: action.payload,
-      }
-    // case 'POST_CREATE':
-    //   return {
-    //     ...state,
-    //   }
-    // case 'GET_DETAIL':
-    //   return {
-    //     ...state,
-    //     detail: action.payload
-    //   }
+      };
+    //---------------------MENTORIAS----------------------------------------
+    case GET_MENTORIAS:
+      return {
+        ...state,
+        mentorias: action.payload,
+        allMentorias: action.payload,
+      };
+    case POST_MENTORIAS:
+      return {
+        ...state,
+        name: !action.payload.error ? action.payload : null,
+        description: action.payload.error ? action.payload.error : null,
+      };
 
-    // case 'SEARCH_BY_NAME':
-    //   return {
-    //     ...state,
-    //     books: action.payload
-    //   }
-
-    // case 'CLEAN_DATA':
-    //   return {
-    //     ...state,
-    //     detail: [],
-    //     tempState: [],
-    //   }
-
-  
-
-    // case GET_CATEGORIES:
-    //   return {
-    //     ...state,
-    //     categories: action.payload
-    //   }
-
-    // case TEMP_STATE:
-    //   return {
-    //     ...state,
-    //     tempState: action.payload
-    //   }
-
-    // // FILTERS
-    // case FILTER_BY_CATEGORY:
-    //   return {
-    //     ...state,
-    //     books: action.payload
-    //   }
-
-    // // SORTERS
-    // case SORT_BY:
-    //   let sortedBooks = [...state.books]
-
-    //   if (action.payload === NEW_FIRST) {
-    //     sortedBooks.sort((a, b) => b.createdAt.localeCompare(a.createdAt))
-    //   }
-
-    //   if (action.payload === NAME_ASC) {
-    //     sortedBooks.sort((a, b) => a.nombre.localeCompare(b.nombre))
-    //   }
-
-    //   if (action.payload === NAME_DESC) {
-    //     sortedBooks.sort((a, b) => b.nombre.localeCompare(a.nombre))
-    //   }
-
-    //   if (action.payload === PRICE_ASC) {
-    //     sortedBooks.sort((a, b) =>
-    //       a.price !== b.price
-    //         ? a.price - b.price
-    //         : a.nombre.localeCompare(b.nombre)
-    //     )
-    //   }
-
-    //   if (action.payload === PRICE_DESC) {
-    //     sortedBooks.sort((a, b) =>
-    //       a.price !== b.price
-    //         ? b.price - a.price
-    //         : b.nombre.localeCompare(a.nombre)
-    //     )
-    //   }
-
-    //   return {
-    //     ...state,
-    //     books: sortedBooks
-    //   }
-
-    // case 'PAGINATION_BOOKS':
-    //   return {
-    //     ...state,
-    //     books: action.payload
-    //   }
-
-    // case 'GET_TOTAL':
-    //   return {
-    //     ...state,
-    //     countBooks: action.payload
-    //   }
-
-    // case 'DELETE_BOOK':
-    //   return {
-    //     ...state,
-    //     delete: action.payload
-    //   }
-
-    // case 'PUT_BOOK_BODY':
-    //   return {
-    //     ...state,
-    //     put: action.payload
-    //   }
-
-    // //BUY
-    // case BUY_BOOK:
-    //   return {
-    //     ...state,
-    //     buy: action.payload
-    //   }
-
-    // //ORDERS
-    // case "ORDER_POST":
-    //   return {
-    //     ...state
-    //   }
-    // case 'HISTORY_ORDER':
-    //   return {
-    //     ...state,
-    //     orders: action.payload,
-    //   }
-    // case GET_DETALLE_ORDER:
-    //   console.log(action.payload)
-    //   return {
-    //     ...state,
-    //     order: action.payload
-    //   }
-    // case "USUARIO_CREATED":
-    //   return {
-    //     ...state,
-    //     booksCreated: action.payload
-    //   }
-    // case "USUARIO_INFO_PROFILE":
-    //   return {
-    //     ...state,
-    //     usuarioProfile: action.payload
-    //   }
-    // //-----------------ADMIN----------------------------------------
-    // case GET_USUARIOS:
-    //   console.log(action.payload)
-    //   return {
-    //     ...state,
-    //     allUsuarios: action.payload,
-
-    //   }
-    // case GET_ORDERS:
-    //   return {
-    //     ...state,
-    //     allOrders: action.payload
-    //   }
-
-
-    // case GET_REVIEW:
-    //   return {
-    //     ...state,
-    //     allReviews: action.payload
-    //   }
-
-    // case DELETE_USER:
-    //   return {
-    //     ...state,
-    //     deleteUserAsAdmin: action.payload
-    //   }
-    // case ADMIN_AN_USER:
-    //   return {
-    //     ...state,
-    //     adminAnUser: action.payload
-    //   }
-    // //----------------------QA---------------
-    // case POST_ANSWER:
-    //   return {
-    //     ...state
-    //   }
-    // case POST_QUESTION:
-    //   return {
-    //     ...state
-    //   }
-    // case GET_QA:
-    //   return {
-    //     ...state,
-    //     questionsAndAnswers: action.payload
-    //   }
-    // case GET_ALL_QUESTIONS:
-    //   return {
-    //     ...state,
-    //     questions: action.payload
-    //   }
-    // case GET_ALL_ANSWERS:
-    //   return {
-    //     ...state,
-    //     answers: action.payload
-    //   }
-    // case GET_ALL_QUESTIONS_COMPRADOR:
-    //   return {
-    //     ...state,
-    //     questionsComprador: action.payload
-    //   }
-    // // -------------------- REVIEW --------------------- 
-    // case "POST_REVIEW":
-    //   return {
-    //     ...state
-    //   }
-
-    // case "GET_REVIEW":
-    //   return {
-    //     ...state,
-    //     review: action.payload
-    //   }
     default:
-      return state
+      return state;
   }
 }
 
-
-
-export default rootReducer
+export default rootReducer;
