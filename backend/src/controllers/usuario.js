59c85b39bb1594ba5fb551238bc680516b7c5a05
EBosch14 <enzo.jbosch@gmail.com>
2024-02-12 10:00:11 -0300

    feat: :sparkles: add edit user endpoint
    ROUTE_API/perfil/:userId

--- backend/src/controllers/usuario.js
+++ backend/src/controllers/usuario.js
@@ -1,62 +1,71 @@
-import { all_users } from '../services/usuarios/all-users.services.js';
-import { autenticarUsuario } from '../services/usuarios/autenticar-usuario.services.js';
-import { confirmUsuario } from '../services/usuarios/confirmar-usuario.services.js';
-import { delete_user } from '../services/usuarios/delete-user.services.js';
-import { nuevaPassword } from '../services/usuarios/nueva-password.services.js';
-import { olvide_password } from '../services/usuarios/olivde-password.services.js';
-import { profile } from '../services/usuarios/perfil.services.js';
-import { register } from '../services/usuarios/register.services.js';
-import { usuarioActual } from '../services/usuarios/usuario.services.js';
-import { response } from '../utils/response.js';
-
+import { all_users } from "../services/usuarios/all-users.services.js";
+import { autenticarUsuario } from "../services/usuarios/autenticar-usuario.services.js";
+import { confirmUsuario } from "../services/usuarios/confirmar-usuario.services.js";
+import { delete_user } from "../services/usuarios/delete-user.services.js";
+import { nuevaPassword } from "../services/usuarios/nueva-password.services.js";
+import { olvide_password } from "../services/usuarios/olivde-password.services.js";
+import { profile } from "../services/usuarios/perfil.services.js";
+import { register } from "../services/usuarios/register.services.js";
+import { updateUser } from "../services/usuarios/update-user.js";
+import { usuarioActual } from "../services/usuarios/usuario.services.js";
+import { response } from "../utils/response.js";
 
 /*************************************************************************/
 // Crear/registrar un usuario
 export const createUser = async ({ body }, res) => {
-    const { email, name, password } = body;
-    const obj = { email, name, password };
-    const user = await register(obj);
-    response(res, 201, user);
+  const { email, name, password } = body;
+  const obj = { email, name, password };
+  const user = await register(obj);
+  response(res, 201, user);
+};
+
+/*************************************************************************/
+//Editar usuario
+export const editUser = async (req, res) => {
+  const { userId } = req.params;
+  const data = req.body;
+  const updatedUser = await updateUser(userId, data);
+  response(res, 201, updatedUser);
 };
 
 /*************************************************************************/
 //Confirmar usuario
 export const confirm = async ({ params }, res) => {
-    const { token } = params;
-    const result = await confirmUsuario(token);
-    response(res, 201, result);
+  const { token } = params;
+  const result = await confirmUsuario(token);
+  response(res, 201, result);
 };
 
 /*************************************************************************/
 //Autenticar información del usuario al momento del login
 //Para autenticar primero debemos confirmar al usuario
 export const authenticate = async ({ body }, res) => {
-    const { email, password } = body;
-    const user = await autenticarUsuario({ email, password })
-    response(res, 201, user);
+  const { email, password } = body;
+  const user = await autenticarUsuario({ email, password });
+  response(res, 201, user);
 };
 
 /*************************************************************************/
 //Traer todos los Usuarios Registrados
 export const allUsers = async (req, res) => {
-    const users = await all_users();
-    response(res, 201, users);
+  const users = await all_users();
+  response(res, 201, users);
 };
 
 /*************************************************************************/
 //Eliminar un usuario por ID
 export const deleteUser = async ({ params }, res) => {
-    const { id } = params;
-    const user = await delete_user(id);
-    response(res, 201, user);
+  const { id } = params;
+  const user = await delete_user(id);
+  response(res, 201, user);
 };
 
 /*************************************************************************/
 //Olvide password de un usuario
 export const olvidePassword = async ({ body }, res) => {
-    const { email } = body;
-    const emailUser = await olvide_password(email);
-    response(res, 201, emailUser);
+  const { email } = body;
+  const emailUser = await olvide_password(email);
+  response(res, 201, emailUser);
 };
 
 // export const comprobarToken = async (req, res) => {
@@ -73,24 +82,24 @@ export const olvidePassword = async ({ body }, res) => {
 // };
 
 export const nuevoPassword = async (req, res) => {
-    const { token } = req.params;
-    const { password } = req.body;
-    const newPassword = await nuevaPassword({ token, password });
-    response(res, 201, newPassword);
+  const { token } = req.params;
+  const { password } = req.body;
+  const newPassword = await nuevaPassword({ token, password });
+  response(res, 201, newPassword);
 };
 
 /*************************************************************************/
 //Mostrar información del perfil que está logueado.
 export const perfil = async ({ usuario }, res) => {
-    const { name } = usuario;
-    const user = await profile(name)
-    response(res, 201, user);
-}
+  const { name } = usuario;
+  const user = await profile(name);
+  response(res, 201, user);
+};
 
 export const usuario = async ({ usuario }, res) => {
-    const { name } = usuario;
-    const user = await usuarioActual({ name });
-    response(res, 201, user);
+  const { name } = usuario;
+  const user = await usuarioActual({ name });
+  response(res, 201, user);
 };
 
 /*************************************************************************/
--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -1,27 +1,35 @@
-import express from 'express';
-import checkAuth from '../middleware/checkAuth.js'
-import { createUser, deleteUser, allUsers, authenticate, confirm, olvidePassword, nuevoPassword, usuario, perfil } from '../controllers/usuario.js'
-import { asyncCatched } from '../utils/asyncCatched.js';
+import express from "express";
+import checkAuth from "../middleware/checkAuth.js";
+import {
+  createUser,
+  deleteUser,
+  allUsers,
+  authenticate,
+  confirm,
+  olvidePassword,
+  nuevoPassword,
+  usuario,
+  perfil,
+  editarPerfil,
+} from "../controllers/usuario.js";
+import { asyncCatched } from "../utils/asyncCatched.js";
 
 const router = express.Router();
 
-router.post('/create', asyncCatched(createUser));
-router.post('/login', asyncCatched(authenticate));
-router.get('/confirm/:token', asyncCatched(confirm));
-router.post('/olvide-password', asyncCatched(olvidePassword));
-router.patch('/olvide-password/:token', asyncCatched(nuevoPassword)); //modificar y guardar password
+router.post("/create", asyncCatched(createUser));
+router.post("/login", asyncCatched(authenticate));
+router.get("/confirm/:token", asyncCatched(confirm));
+router.post("/olvide-password", asyncCatched(olvidePassword));
+router.patch("/olvide-password/:token", asyncCatched(nuevoPassword)); //modificar y guardar password
 
-router.get('/perfil', checkAuth, asyncCatched(perfil)); //Ingresar al perfil solo si es el usuario
-router.get('/actual', checkAuth, asyncCatched(usuario));
-
-
+router.get("/perfil", checkAuth, asyncCatched(perfil)); //Ingresar al perfil solo si es el usuario
+router.get("/actual", checkAuth, asyncCatched(usuario));
+router.patch("/perfil/:userId", checkAuth, asyncCatched(editarPerfil));
 
 //Admin
-router.delete('/delete/:id', asyncCatched(deleteUser));
-
-
+router.delete("/delete/:id", asyncCatched(deleteUser));
 
 //SuperADMIN
-router.get('/allusers/', asyncCatched(allUsers));
+router.get("/allusers/", asyncCatched(allUsers));
 
 export default router;
