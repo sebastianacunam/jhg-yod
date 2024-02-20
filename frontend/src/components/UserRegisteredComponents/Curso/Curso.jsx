83389de3c5662dbf97684716d01dd9d5923fbd79
Davidoar15 <davidoar15@gmail.com>
2024-02-20 15:36:41 -0300

    Quitar style prop en Curso.jsx

--- frontend/src/components/UserRegisteredComponents/Curso/Curso.jsx
+++ frontend/src/components/UserRegisteredComponents/Curso/Curso.jsx
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom'
 
 export default function Curso({id, name, description}) {
   return (
-    <div style={{ border: "2px solid red" }} className='bg-curso'>
+    <div className='bg-curso'>
       <NavLink to={`/detalles/${id}`}>
-        <div style={{ border: "3px solid blue" }} className='each-curso'>
+        <div className='each-curso'>
               <p>{name}</p>
               <p>{description}</p>
         </div>
