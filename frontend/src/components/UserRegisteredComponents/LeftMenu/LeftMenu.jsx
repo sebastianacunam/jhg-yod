7674c2fe7820eefa8c66436cc0500e14d5e5eaed
enzosamojedny <enzosamojedny@gmail.com>
2024-03-04 01:40:24 -0300

    bug fixes in leftMenu

--- frontend/src/components/UserRegisteredComponents/LeftMenu/LeftMenu.jsx
+++ frontend/src/components/UserRegisteredComponents/LeftMenu/LeftMenu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { MdDashboard } from "react-icons/md";
 import { MdOutlineCases } from "react-icons/md";
 import { AiFillNotification } from "react-icons/ai";
@@ -27,7 +27,6 @@ export default function LeftMenu({ isOpen }) {
   const dispatch = useDispatch();
   const usuarioAct = useSelector((state) => state.usuarioActual);
   // const token = useSelector((state) => state.refreshToken);
-  const [hidden, setHidden] = useState(false);
   const token = localStorage.getItem("token");
   useEffect(() => {
     token ? dispatch(usuarioActual()) : dispatch(null);
@@ -40,84 +39,73 @@ export default function LeftMenu({ isOpen }) {
   }
 
   return (
-    <motion.div
-      hidden={hidden}
-      initial={false}
-      onAnimationStart={() => setHidden(false)}
-      onAnimationComplete={() => setHidden(!isOpen)}
-      animate={{ width: isOpen ? 500 : 0 }}>
-      <div className='dashboard-left'>
-        {/* //menu */}
-        <motion.div
-          className='inner-dashboard-left'
-          animate={{ width: isOpen ? "20%" : 0 }}
-          transition={{ duration: 0.5 }}>
-          <div className='welcome-dashboard-wrapper'>
-            <p>Bienvenido a tu dashboard</p>
-            <p>{usuarioAct?.name}</p>
-          </div>
-          <ul className='ul-li-items'>
-            <li className='li-items'>
-              <Link to='/dashboard' className='nav-link'>
-                <MdDashboard />
-                Dashboard
-              </Link>
-            </li>
-            <li className='li-items '>
-              <CursosNavbar />
-            </li>
-            <li className='li-items'>
-              <BootcampsNavbar />
-            </li>
-            <li className='li-items'>
-              <MentoriasNavbar />
-            </li>
-            <li className='li-items'>
-              <Link to='/anuncios' className='nav-link'>
-                <AiFillNotification />
-                Anuncios
-              </Link>
-            </li>
-            <li className='li-items'>
-              <Link to='/beneficios' className='nav-link'>
-                <AiFillRocket />
-                Beneficios
-              </Link>
-            </li>
-            <li className='li-items'>
-              <div className='nav-link'>
-                <BsPeopleFill />
-                <ComunidadesNavbar />
-              </div>
-            </li>
-            <li className='li-items'>
-              <Link to='/bolsa-empleo' className='nav-link'>
-                <MdOutlineCases />
-                Bolsa de empleos
-              </Link>
-            </li>
-
-            <li className='li-items'>
-              <Link to='/perfil' className='nav-link'>
-                <FaUser />
-                Perfil
-              </Link>
-            </li>
-            <div className='i-d-l-btn' onClick={() => logOut()}>
-              <Link to='/login'>
-                <div className='btn-logout'>
-                  <img
-                    className='logout-icon'
-                    src={logout}
-                    alt='img not found'
-                  />
-                  <p>Logout</p>
-                </div>
-              </Link>
+    <div className='dashboard-left'>
+      {/* //menu */}
+      <motion.div
+        className='inner-dashboard-left'
+        animate={{ width: isOpen ? "0%" : "18.75%" }}
+        transition={{ duration: 0.5 }}>
+        <div className='welcome-dashboard-wrapper'>
+          <p>Bienvenido a tu dashboard</p>
+          <p>{usuarioAct?.name}</p>
+        </div>
+        <ul className='ul-li-items'>
+          <li className='li-items'>
+            <Link to='/dashboard' className='nav-link'>
+              <MdDashboard />
+              Dashboard
+            </Link>
+          </li>
+          <li className='li-items '>
+            <CursosNavbar />
+          </li>
+          <li className='li-items'>
+            <BootcampsNavbar />
+          </li>
+          <li className='li-items'>
+            <MentoriasNavbar />
+          </li>
+          <li className='li-items'>
+            <Link to='/anuncios' className='nav-link'>
+              <AiFillNotification />
+              Anuncios
+            </Link>
+          </li>
+          <li className='li-items'>
+            <Link to='/beneficios' className='nav-link'>
+              <AiFillRocket />
+              Beneficios
+            </Link>
+          </li>
+          <li className='li-items'>
+            <div className='nav-link'>
+              <BsPeopleFill />
+              <ComunidadesNavbar />
             </div>
-          </ul>
-        </motion.div>
-      </div>
-    </motion.div>
+          </li>
+          <li className='li-items'>
+            <Link to='/bolsa-empleo' className='nav-link'>
+              <MdOutlineCases />
+              Bolsa de empleos
+            </Link>
+          </li>
+
+          <li className='li-items'>
+            <Link to='/perfil' className='nav-link'>
+              <FaUser />
+              Perfil
+            </Link>
+          </li>
+          <div className='i-d-l-btn' onClick={() => logOut()}>
+            <Link to='/login'>
+              <div className='btn-logout'>
+                <img className='logout-icon' src={logout} alt='img not found' />
+                <p>Logout</p>
+              </div>
+            </Link>
+          </div>
+        </ul>
+      </motion.div>
+    </div>
   );
 }
