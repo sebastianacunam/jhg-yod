d4013ada303529a0d7f69ba5b4113d6d0aeee287
Diiegote <diego.elprado22@gmail.com>
2024-03-05 13:50:43 -0300

    Se crea una clase para manejar los errores en el front. Se agrega alerta de error en el login. Se agrega alerta de error en confirmAccount.

--- backend/src/services/usuarios/autenticar-usuario.services.js
+++ backend/src/services/usuarios/autenticar-usuario.services.js
@@ -8,7 +8,7 @@ export const autenticarUsuario = async ({ email, password }, res) => {
    //Comprobar si existe el usuario
    const usuario = await Usuario.findOne({ email }).select('+password')
    if (!usuario) {
-      throw new ClientError('El usuario no existe', 404);
+      throw new ClientError('Credenciales inválidas', 401);
    }
 
    //Comprobar si el usuario está confirmado
@@ -19,7 +19,7 @@ export const autenticarUsuario = async ({ email, password }, res) => {
    //Comprobar su password, previamente hasheada en el modelo Usuario.js
    const validatePassword = await usuario.comprobarPassword(password);
    if (!validatePassword) {
-      throw new ClientError('La contraseña es incorrecta', 401);
+      throw new ClientError('Credenciales inválidas', 401);
    }
 
    const { token, expiresIn } = generateJWT(usuario._id);
--- frontend/src/redux/actions/actionEmpleos.js
+++ frontend/src/redux/actions/actionEmpleos.js
@@ -1,4 +1,5 @@
 import clienteAxios from '../../config/clienteAxios'
+import { ErrorHandler } from '../../utils/errorHandler';
 import { GET_EMPLEOS } from '../utils/constants';
 
 
@@ -6,12 +7,15 @@ export function getEmpleos(query = '') {
    return async function (dispatch) {
       try {
          const resp = await clienteAxios(`empleos/remotive${query}`);
+         if (resp.data.error === true) {
+            throw new ErrorHandler("Hubo un error al cargar los empleos");
+         }
          return dispatch({
             type: GET_EMPLEOS,
             payload: resp.data.data,
          });
       } catch (error) {
-         console.log(error.message);
+         throw new ErrorHandler("Hubo un error al cargar los empleos");
       }
    }
 }
